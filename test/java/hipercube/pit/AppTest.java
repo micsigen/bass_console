f44aaaeb6a555cbb2f08f6620bcef0b111ad480b
bmajor <major.balazs@icloud.com>
2017-05-19 05:55:34 +0200

    Extend AppTest to survive all PIT test.

--- test/java/hipercube/pit/AppTest.java
+++ test/java/hipercube/pit/AppTest.java
@@ -22,6 +22,30 @@ public class AppTest {
     }
 
     @Test
+    public void test7() {
+        final int max = new App().ValidPassword("1Aa3456");
+        assertEquals(max, 1);
+    }
+
+    @Test
+    public void test8() {
+        final int max = new App().ValidPassword("1Aa45678");
+        assertEquals(max, 0);
+    }
+
+    @Test
+    public void test33() {
+        final int max = new App().ValidPassword("123456789012345A789012345a7890123");
+        assertEquals(max, 1);
+    }
+
+    @Test
+    public void test32() {
+        final int max = new App().ValidPassword("123456789012345A789012345a789012");
+        assertEquals(max, 0);
+    }
+
+    @Test
     public void testLower() {
         final int lower = new App().ValidPassword("AAAAAAA12345678");
         assertEquals(lower, 2);
@@ -40,8 +64,57 @@ public class AppTest {
     }
 
     @Test
+    public void testBadPasswd1() {
+        final int digit = new App().ValidPassword("badpassword1");
+        assertEquals(digit, 2);
+    }
+
+    @Test
+    public void testBadPasswd2() {
+        final int digit = new App().ValidPassword("badpasswordA");
+        assertEquals(digit, 2);
+    }
+
+    @Test
+    public void testBadPasswd3() {
+        final int digit = new App().ValidPassword("123456789A");
+        assertEquals(digit, 2);
+    }
+
+    @Test
+    public void testBadPasswd4() {
+        final int digit = new App().ValidPassword("123456789a");
+        assertEquals(digit, 2);
+    }
+
+    @Test
+    public void testBadPasswd5() {
+        final int digit = new App().ValidPassword("ABCDEFGHIJKL1");
+        assertEquals(digit, 2);
+    }
+
+    @Test
+    public void testBadPasswd6() {
+        final int digit = new App().ValidPassword("ABCDEFGHIJKLa");
+        assertEquals(digit, 2);
+    }
+
+    @Test
     public void testValid() {
         final int valid = new App().ValidPassword("GoodPassword123");
         assertEquals(valid, 0);
     }
+
+    @Test
+    public void testValid2() {
+        final int valid = new App().ValidPassword("SecondGoodPassword123!");
+        assertEquals(valid, 0);
+    }
+
+    @Test
+    public void testValid3() {
+        final int valid = new App().ValidPassword("Secondgoodpassword1");
+        assertEquals(valid, 0);
+    }
+
 }
